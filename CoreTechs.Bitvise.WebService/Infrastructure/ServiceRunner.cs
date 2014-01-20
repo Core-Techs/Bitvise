8efcdca7556ea8c76d1bfa88e97254983a0ea89c
Ronnie Overby <roverby@core-techs.net>
2014-01-20 15:37:19 -0500

    boilerplate for windows service

---
+++ CoreTechs.Bitvise.WebService/Infrastructure/ServiceRunner.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+using System.ServiceProcess;
+
+namespace CoreTechs.Bitvise.WebService.Infrastructure
+{
+    /// <summary>
+    /// Run your windows services as console apps during development!
+    /// </summary>
+    public class ServiceRunner
+    {
+        public ServiceBase[] Services { get; private set; }
+        public string[] ServiceArgs { get; private set; }
+
+        public ServiceRunner(string[] args = null, params ServiceBase[] services)
+        {
+            ServiceArgs = args;
+            Services = services;
+        }
+
+        public void Start(string exitPrompt = "Press Enter to stop the service...")
+        {
+            if (Environment.UserInteractive)
+            {
+                var onStart = typeof(ServiceBase)
+                    .GetMethod("OnStart", BindingFlags.Instance | BindingFlags.NonPublic);
+
+                foreach (var svc in Services)
+                    onStart.Invoke(svc, BindingFlags.Default, null, new object[] { ServiceArgs }, null);
+
+                Console.WriteLine(exitPrompt);
+                Console.ReadLine();
+            }
+            else
+            {
+                ServiceBase.Run(Services);
+            }
+        }
+
+        public void Stop()
+        {
+            foreach (var svc in Services)
+                svc.Stop();
+        }
+    }
+}
--- CoreTechs.Bitvise.WebService/Program.cs
+++ CoreTechs.Bitvise.WebService/Program.cs
@@ -1,15 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CoreTechs.Bitvise.WebService;
+using CoreTechs.Bitvise.WebService.Infrastructure;
+using CoreTechs.Logging;
+using System;
+using System.ServiceProcess;
 
-namespace CoreTechs.Bitvise.WebService
+namespace AFS.BatchService
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        private static readonly Logger Log = SetupLogging().CreateLogger();
+
+        private static LogManager SetupLogging()
         {
+            return LogManager.Global = LogManager.Configure("logging");
+        }
+
+        private static void Main(string[] args)
+        {
+            var singleGlobalInstance = Attempt.Get(() => new SingleGlobalInstance()).Value;
+            if (singleGlobalInstance == null)
+            {
+                if (Environment.UserInteractive)
+                {
+                    Console.WriteLine("Another instance of the application is running. Press any key to exit.");
+                    Console.ReadKey();
+                }
+                return;
+            }
+
+            using (singleGlobalInstance)
+            {
+
+                var svc = new BitviseService();
+                var svcRunner = new ServiceRunner(args, svc);
+                svcRunner.Start();
+
+                // wait for any straggling async log entries
+                LogManager.Global.Dispose();
+            }
         }
     }
 }
